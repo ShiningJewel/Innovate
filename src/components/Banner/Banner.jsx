e867bf11dd09228568d5cb00e9cb45cdd25bca20
ShiningJewel <shiningjewel317@gmail.com>
2025-06-09 23:08:33 +0600

    innovate - responsive navbar done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,8 +12,8 @@ const Banner = () => {
       <Container>
         <div className='relative'>
           <div className='text-center '>
-            <h1 className='w-[796px] m-auto font-secondary font-bold text-[56px] leading-[64px] text-white'>Get your work done with Management Tool</h1>
-            <p className='w-[467px] m-auto font-secondary font-regular text-lg leading-8 text-white mt-5 mb-8'>The world's first project management platform that connects everything</p>
+            <h1 className='w-auto md:w-[796px] m-auto font-secondary font-bold text-[33px] md:text-[56px] leading-[50px] md:leading-[64px] text-white'>Get your work done with Management Tool</h1>
+            <p className='w-auto md:w-[467px] m-auto font-secondary font-regular text-lg leading-8 text-white mt-5 mb-8'>The world's first project management platform that connects everything</p>
             <div className='relative'>
               <input type="text" className='w-[480px] outline-0 bg-white rounded-[6px] placeholder:text-[#B4B6C4] font-secondary font-regular text-base leading-[26px] pl-6 py-5' placeholder='Your business email' />
               <a className='absolute top-[4px] right-[475px] bg-primary py-[15px] px-7 font-priamry font-medium text-lg rounded-[6px] text-[#2C2643]'>Try for free</a>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
 import Container from '../Layout/Container'
+import { FaBars } from "react-icons/fa";
+import { useState } from 'react';
+import { RxCross2 } from "react-icons/rx";
+
 const Navbar = () => {
+
+const [show, setShow] = useState(false);
+const handleClick = () =>{
+  setShow(true)
+}
+
+
+
   return (
-    <div className='absolute top-0 left-0 w-full py-10'>
+    <div className='absolute top-0 left-0 w-full py-10 px-5 md:px-0 '>
       <Container>
-                <div className='flex justify-between items-center'>
-        <div>
+                <div className='md:flex justify-between items-center'>
+        <div className='flex items-center justify-between'>
           <img src={logo} alt="#logo" />
+          <div>
+            <FaBars onClick={handleClick} className='block md:hidden text-3xl text-white' />
+          </div>
         </div>
         <div>
-          <ul className='flex items-center font-primary font-medium text-white text-lg gap-x-[50px]'>
+          <ul className='hidden md:flex items-center font-primary font-medium text-white text-lg gap-x-[50px]'>
             <li><a href="">Home</a></li>
             <li><a href="">Features</a></li>
             <li><a href="">Service</a></li>
@@ -18,12 +33,38 @@ const Navbar = () => {
             <li><a href="">Blog</a></li>
           </ul>
         </div>
-        <div className='flex items-center gap-x-[37px]'>
+        <div className='hidden md:flex items-center gap-x-[37px]'>
           <p className='font-priamry font-medium text-lg text-white '>Login</p>
-          <a className=' bg-primary py-[17px] px-12 font-priamry font-medium text-lg rounded-[6px] text-secondary'>Register</a>
+         <div>
+           <button className=' bg-primary py-[17px] px-12 font-priamry font-medium text-lg rounded-[6px] text-secondary'>Register</button>
+         </div>
         </div>
         </div>
       </Container>
+      {
+        show &&
+        <div className='bg-white w-full h-screen absolute flex top-0 left-0 text-center justify-center items-center z-[999]'>
+          <div className='absolute top-0 left-0'>
+          <RxCross2 onClick={()=>setShow(false)} className='text-3xl text-[#b8a5a5f5]' />
+          </div>
+                  <div>
+          <ul className='items-center font-primary font-medium text-black text-lg'>
+            <li className='mb-5'><a href="">Home</a></li>
+            <li className='mb-5'><a href="">Features</a></li>
+            <li className='mb-5'><a href="">Service</a></li>
+            <li className='mb-5'><a href="">Pages</a></li>
+            <li className='mb-5'><a href="">Blog</a></li>
+          </ul>
+          <div className=' items-center'>
+          <p className='mb-5 font-priamry font-medium text-lg text-black '>Login</p>
+         <div>
+           <button className=' bg-primary py-[17px] px-12 font-priamry font-medium text-lg rounded-[6px] text-secondary'>Register</button>
+         </div>
+        </div>
+        </div>
+        
+        </div>
+      }
     </div>
   )
 }
