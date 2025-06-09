3360ca49da8d9a17cc142ba12878e3e6f18f27c6
ShiningJewel <shiningjewel317@gmail.com>
2025-06-09 10:12:09 +0600

    innovate - intergration section update

--- src/components/Intergration/Intergration.jsx
+++ src/components/Intergration/Intergration.jsx
@@ -8,20 +8,26 @@ import frame5 from '../../assets/frame5.png'
 import frame6 from '../../assets/frame6.png'
 import frame7 from '../../assets/frame7.png'
 import frame8 from '../../assets/frame8.png'
+import intergrationshadow from '../../assets/intergrationshadow.png'
 
 const Intergration = () => {
   return (
     <section className='pt-[55px]  bg-[#F4FAFA]'>
       <Container>
-        <div className='pt-[124px] '>
+        <div className='relative'>
+          <div className='absolute bottom-[61px] left-[-156px]'>
+            <img src={intergrationshadow} alt="" />
+          </div>
          <div className='flex justify-between'>
-           <div>
+           <div className='w-1/2 mt-[173px]'>
             <h4 className='font-primary font-medium text-xl text-[#FF7364]'>Integrations</h4>
             <h6 className='w-[587px] font-secondary font-bold text-[46px] leading-[58px] text-titleColor mt-[4px] mb-5 '>Easily integrate with your favorite apps</h6>
             <p  className='w-[533px] font-primary text-paraColor font-regular text-lg leading-[32px]'>App integration, in a general sense, is the process of bringing resources or capabilities from one application to another. As the world of apps continues to evolve, app integration is becoming expected in many contexts.</p>
               <button className=' bg-secondary py-[20px] px-12 font-priamry font-medium text-lg rounded-[6px] text-white mt-10'>Get Started</button>
           </div>
-          <div className='w-[752px] h-[544px] shadow-[0_0_0_rgba(0,0,0,0.25)]'>
+          <div>
+            <div className='w-1/2 mt-[124px] mb-[120px]'>
+            <div className='w-[752px] h-[544px] shadow-[0_0_0_rgba(0,0,0,0.25)]'>
             <div  className='flex justify-between'>
               <div className='flex justify-center items-center w-[332px] h-[143px] shadow-[0px_0px_0px_rgba(0,0,0,0.25)] bg-white'>
                 <img className=' w-[202px] m-auto' src={frame1} alt="#frame1" />
@@ -52,6 +58,8 @@ const Intergration = () => {
                 <img className='  m-auto' src={frame8} alt="#frame8" />
               </div>
             </div>
+          </div>
+          </div>
           </div>
          </div>
         </div>
